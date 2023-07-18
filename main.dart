4c7bb6ac9f128cbcb7cba8c839f2474f2acb83d0
abdul razzaq <ar8937562@gmail.com>
2023-07-18 02:19:43 -0700

    Dark and Light theme app by Provider and changes in main file

--- main.dart
+++ main.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import 'package:provider_flutter/Providerrr/count_foundation.dart';
 import 'package:provider_flutter/Providerrr/example_two_provider.dart';
 import 'package:provider_flutter/Providerrr/favourite_provider/favorite_provider.dart';
+import 'package:provider_flutter/Providerrr/theme_changer_provider.dart';
 import 'package:provider_flutter/Screens/count_example.dart';
+import 'package:provider_flutter/Screens/dark_theme.dart';
 import 'package:provider_flutter/Screens/example_two.dart';
 import 'package:provider_flutter/Screens/favorite/favorite_app.dart';
 import 'package:provider_flutter/home_screen.dart';
@@ -20,20 +22,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // final themeChanger = Provider.of<ThemeChangerProvider>(context);
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => CountFoundationProvider()),
-        ChangeNotifierProvider(create: (_) => ExampleTwoProvider()),
-        ChangeNotifierProvider(create: (_) => FavouriteProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const FavoriteApp(),
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (_) => CountFoundationProvider()),
+          ChangeNotifierProvider(create: (_) => ExampleTwoProvider()),
+          ChangeNotifierProvider(create: (_) => FavouriteProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeChangerProvider()),
+        ],
+        child: Builder(builder: (context) {
+          final themeChanger = Provider.of<ThemeChangerProvider>(context);
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            themeMode: themeChanger.themeMode,
+            theme: ThemeData(
+              brightness: Brightness.light,
+              primarySwatch: Colors.deepOrange,
+            ),
+            darkTheme: ThemeData(
+                appBarTheme: const AppBarTheme(
+                  backgroundColor: Colors.teal,
+                ),
+                brightness: Brightness.dark),
+            home: const DarkTheme(),
+          );
+        }));
   }
 }
